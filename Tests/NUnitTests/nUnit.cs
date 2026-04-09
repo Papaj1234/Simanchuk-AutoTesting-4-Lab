de626279aa48ccc09c685f61738c6c68e00c042a
Papaj1234 <germann2007simm@gmail.com>
2026-04-09 16:37:57 +0300

    adding tests

---
+++ Tests/NUnitTests/nUnit.cs
@@ -0,0 +1,63 @@
+using NUnit.Framework;
+using SeleniumPatterns.Builders;
+using SeleniumPatterns.Core;
+using SeleniumPatterns.Factory;
+
+namespace SeleniumPatterns.Tests.NUnitTests;
+
+[TestFixture]
+[Parallelizable(ParallelScope.Children)]
+public class EhuUniversityNUnitTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        var options = new ChromeOptionsBuilder().WithMaximizedWindow().Build();
+
+        WebDriverManager.Instance.Initialize(options);
+    }
+
+    [TearDown]
+    public void Teardown() => WebDriverManager.Instance.Quit();
+
+    [Test]
+    [Category("Navigation")]
+    public void NavigateToAboutPage_ShouldOpenCorrectUrl()
+    {
+        var aboutPage = PageFactory.CreateHomePage().Open().NavigateToAbout();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(aboutPage.CurrentUrl, Is.EqualTo("https://en.ehuniversity.lt/about/"));
+            Assert.That(aboutPage.GetHeadingText(), Is.EqualTo("About"));
+        });
+    }
+
+    private static IEnumerable<string> SearchTerms() => new[] { "study programs", "admission" };
+
+    [Test]
+    [Category("Search")]
+    [TestCaseSource(nameof(SearchTerms))]
+    public void SearchByTerm_ShouldRedirectWithQueryInUrl(string searchTerm)
+    {
+        var results = PageFactory.CreateHomePage().Open().Search(searchTerm);
+        Assert.That(results.ResultUrl, Does.Contain(searchTerm.Replace(" ", "+")));
+    }
+
+    [Test]
+    [Category("Localization")]
+    public void SwitchLanguageToLithuanian_ShouldChangeUrl()
+    {
+        var home = PageFactory.CreateHomePage().Open();
+        home.SwitchToLithuanian();
+        Assert.That(home.CurrentUrl, Does.Contain("lt.ehuniversity.lt"));
+    }
+
+    [Test]
+    [Category("Content")]
+    public void ContactPage_ShouldContainExpectedEmail()
+    {
+        var contact = PageFactory.CreateContactPage().Open();
+        Assert.That(contact.GetContactListText(), Does.Contain("franciskscarynacr@gmail.com"));
+    }
+}
---
+++ Tests/XUnitTests/xUnit.cs
@@ -0,0 +1,65 @@
+using SeleniumPatterns.Builders;
+using SeleniumPatterns.Core;
+using SeleniumPatterns.Factory;
+using Xunit;
+
+namespace SeleniumPatterns.Tests.XUnitTests;
+
+[Collection("Sequential")]
+public class EhuUniversityXUnitTests : IDisposable
+{
+    public EhuUniversityXUnitTests()
+    {
+        var options = new ChromeOptionsBuilder().WithMaximizedWindow().Build();
+
+        WebDriverManager.Instance.Initialize(options);
+    }
+
+    public void Dispose() => WebDriverManager.Instance.Quit();
+
+    [Fact]
+    [Trait("Category", "Navigation")]
+    public void NavigateToAboutPage_ShouldOpenCorrectUrl()
+    {
+        var aboutPage = PageFactory.CreateHomePage().Open().NavigateToAbout();
+
+        Assert.Equal("https://en.ehuniversity.lt/about/", aboutPage.CurrentUrl);
+        Assert.Equal("About", aboutPage.GetHeadingText());
+    }
+
+    public static IEnumerable<object[]> SearchTerms =>
+        new List<object[]>
+        {
+            new object[] { "study programs" },
+            new object[] { "admission" }
+        };
+
+    [Theory]
+    [MemberData(nameof(SearchTerms))]
+    [Trait("Category", "Search")]
+    public void SearchByTerm_ShouldRedirectWithQueryInUrl(string searchTerm)
+    {
+        var results = PageFactory.CreateHomePage().Open().Search(searchTerm);
+        Assert.Contains(searchTerm.Replace(" ", "+"), results.ResultUrl);
+    }
+
+    [Fact]
+    [Trait("Category", "Localization")]
+    public void SwitchLanguageToLithuanian_ShouldChangeUrl()
+    {
+        var home = PageFactory.CreateHomePage().Open();
+        home.SwitchToLithuanian();
+        Assert.Contains("lt.ehuniversity.lt", home.CurrentUrl);
+    }
+
+    [Fact]
+    [Trait("Category", "Content")]
+    public void ContactPage_ShouldContainExpectedEmail()
+    {
+        var contact = PageFactory.CreateContactPage().Open();
+        Assert.Contains("franciskscarynacr@gmail.com", contact.GetContactListText());
+    }
+}
+
+[CollectionDefinition("Sequential", DisableParallelization = true)]
+public class SequentialCollection { }
